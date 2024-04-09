9f0f9236eb2361abaaba89d2b356504b0c4d3e8b
eunchan lee <jsmith26@gmail.com>
2024-04-09 09:47:55 +0900

    fork

--- config/settings.py
+++ config/settings.py
@@ -80,21 +80,14 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR/'db.sqlite3'
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'lion_eunchan', # 공용 DB 이름
+        'USER': 'postgres', # 유저이름
+        'PASSWORD': 'G=nf_-}EKl', # 비밀번호
+        'HOST': 'hanslab.org',  # 또는 PostgreSQL 서버의 IP 주소
+        'PORT': '25432',       # PostgreSQL의 기본 포트 번호
     }
 }
-
-# DATABASES = {
-#     'default': {
-#         'ENGINE': 'django.db.backends.postgresql',
-#         'NAME': 'lion_imjonghan',
-#         'USER': 'postgres',
-#         'PASSWORD': 'G=nf_-}EKl',
-#         'HOST': 'hanslab.org',  # 또는 PostgreSQL 서버의 IP 주소
-#         'PORT': '25432',       # PostgreSQL의 기본 포트 번호
-#     }
-# }
 
 
 # Password validation
